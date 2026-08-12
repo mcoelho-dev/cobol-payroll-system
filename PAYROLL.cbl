9cdca754c7e0ba879b3f61f39f80043a496f1293
mcoelho-dev <migueloliveiracoelho2003@gmail.com>
2026-08-12 09:33:21 -0300

    Created employees summary file, and expanded on the payroll.cbl file

--- PAYROLL.cbl
+++ PAYROLL.cbl
@@ -1,17 +1,26 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. EMPLOYEES.
+       PROGRAM-ID. PAYROLL.
        AUTHOR. MIGUEL COELHO.
 
        ENVIRONMENT DIVISION.
-       FILE CONTROL.
        INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+       SELECT EMPLOYEES-FILE ASSIGN TO "employees.dat".
+       SELECT EMPLOYEES-SUMMARY ASSIGN TO "payroll_report.txt".
 
        DATA DIVISION.
+       FILE SECTION.
+       FD  EMPLOYEES-FILE.
+       01  EMP-DATA.
+           05 EMP-ID            PIC 9(4).
+           05 EMP-NAME          PIC X(30).
+           05 EMP-BASE-SALARY   PIC 9(7)V99.
+           05  EMP-EXTRA-HOURS  PIC 9(3).
+           05  EMP-HOURLY-RATE  PIC 9(5)V99.
+           05  EMP-BONUS        PIC 9(7)V99.
+           05  EMP-HEALTH-PLAN  PIC 9(5)V99.
+
+       FD  EMPLOYEES-SUMMARY.
+       01  SUMMARY              PIC X(80).
+
        WORKING-STORAGE SECTION.
-       EMP-ID             PIC 9(4).
-       EMP-NAME           PIC X(30).
-       EMP-BASE-SALARY    PIC 9(7)V99.
-       EMP-EXTRA-HOURS    PIC 9(3).
-       EMP-HOURLY-RATE    PIC 9(5)V99.
-       EMPT-BONUS         PIC 9(7)V99.
-       EMP-HEALTH-PLAN    PIC 9(5)V99.
